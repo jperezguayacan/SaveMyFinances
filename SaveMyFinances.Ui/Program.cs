ab86a774e5c70c9a169c28e68cf453d9ad5fe3bf
Jeronimo P Guayacan <jperezguayacan@hotmail.com>
2025-11-24 21:28:14 -0500

    Add Project Structure Add Api,Ui,Maui,Shared

---
+++ SaveMyFinances.Ui/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SaveMyFinances.Ui;
+using MudBlazor.Services;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https:localhost:7254") });
+
+await builder.Build().RunAsync();
